58966b6a6ed80576e1f63502a8f5262381f47d58
dBCooper2 <t.rowland711@gmail.com>
2024-03-18 13:50:01 -0500

    added outline

---
+++ tr-dev/app/page.tsx
@@ -0,0 +1,21 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Trevor Rowland</h1>
+      <h2>Business Intelligence, Data Analytics</h2>
+      <p>
+      Analytical Thinker studying the intersection of business, 
+      computer science and mathematics. Majoring in Business 
+      Intelligence and Data Analytics with a Mathematics minor 
+      at Tennessee Tech's College of Business, and studying 
+      financial analysis using Python in my free time.
+      </p>
+      <div>
+        LinkedinButton, GithubButton, DownloadResumeButton, ContactButton
+      </div>
+
+    </div>
+  );
+};
+
+export default HomePage;
