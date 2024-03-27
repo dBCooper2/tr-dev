5625d4843de2a2e5a71c266e5ae87a14a6045c36
dBCooper2 <t.rowland711@gmail.com>
2024-03-27 14:15:13 -0500

    revert abstract

--- tr-dev/components/Abstract.jsx
+++ tr-dev/components/Abstract.jsx
@@ -1,29 +1,10 @@
-import React from "react";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkReact from "remark-react";
-import fs from "fs";
-import path from "path";
-
-const Abstract = ({ content }) => {
-  const renderMarkdown = () => {
-    const processor = unified().use(remarkParse).use(remarkReact);
-    const htmlContent = processor.processSync(content).result;
-    return <div>{htmlContent}</div>;
-  };
-
-  return <div>{renderMarkdown()}</div>;
+const Abstract = () => {
+  return (
+    <div>
+      This is the Abstract Section! This page includes the abstract along with
+      any additional formatting.
+    </div>
+  );
 };
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "resumeFiles", "abstract.md");
-  const fileContent = fs.readFileSync(filePath, "utf8");
-
-  return {
-    props: {
-      content: fileContent,
-    },
-  };
-}
 
 export default Abstract;
